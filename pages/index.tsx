9a0b7add1060b9d67d810fee9e14017487278c2d
Ryan Ponce <ryan@ponce.la>
2020-09-24 21:44:38 -0700

    Add styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function IndexPage() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <GreetingText>Hi, I'm Ryan</GreetingText>
+    </div>
+  );
 }
+
+const GreetingText = styled.p`
+  color: ${(props) => props.theme.colors.main};
+`;
